dd7744c63b4a1e30e4f58c44a3158069b83c49bc
Eduardo Almeida <almeedusa@gmail.com>
2026-04-29 20:54:41 -0300

    feat: create Home view layout structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="invite">
+            <h3>Foto convite</h3>
+        </div>
+        <div class="whatsapp-button">
+            <h3>Botão do grupo</h3>
+        </div>
+        <div class="photo-carousel">
+            <h3>Carrossel de imagens</h3>
+        </div>
+        <div class="logo">
+            <h3>Logo</h3>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+
+<style scoped></style>
