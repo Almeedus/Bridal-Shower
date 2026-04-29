3f585d26392af5e2abb1be09863e044012e38b67
Eduardo Almeida <almeedusa@gmail.com>
2026-04-29 20:54:14 -0300

    feat: configure Vue Router with Home and List views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ListView from '@/views/ListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: "/", component: HomeView},
+    {path: "/list", component: ListView}
+  ],
 })
 
 export default router
