a2d7a9ea1c2f1200931f943943fce6ba48598645
Eduardo Almeida <almeedusa@gmail.com>
2026-05-04 20:15:30 -0300

    feat: create ListItem component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,46 @@
+<script setup>
+    defineProps({
+        nameItem: String,
+        imageItem: String
+    })
+</script>
+
+<template>
+    <div class="card"
+    @click="console.log('Funcionando')">
+        <img :src="imageItem">
+        <p>{{nameItem}}</p>
+    </div>
+</template>
+
+<style scoped>
+
+
+.card {
+    background-color: #53674d;
+    width: 120px;
+    padding: 3px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 5%;
+    gap: 20px;
+    cursor: pointer;
+}
+
+.card img {
+    width: 100px;
+    height: 100px;
+    border-radius: 5%;
+    margin-top: 10px;
+    object-fit: cover;
+}
+
+.card p {
+    color: #fff;
+    font-size: 16px;
+    margin-top: -10px;
+    margin-bottom: 10px;
+    text-align: center;
+}
+</style>
