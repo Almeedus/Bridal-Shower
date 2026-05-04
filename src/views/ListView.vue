b48e64f8a778578083fbfe258c3d61bcc4177079
Eduardo Almeida <almeedusa@gmail.com>
2026-05-04 20:18:02 -0300

    feat: Create a list of cards, perform tests, and define styles within the scope.

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,12 +1,110 @@
 <script setup>
-    import LogoImage from '../assets/LogoImage.jpg'
+    import LogoImage from '../assets/LogoImage.png'
+    import ListItem from '@/components/ListItem.vue';
+
+    import JogoCopo from '../assets/items/JogoCopo.webp'
+    import JogoPrato from '../assets/items/JogoPrato.webp'
+    const items = [
+        {
+            'id': 0,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+        {
+            'id': 1,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 2,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 3,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+        {
+            'id': 0,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+        {
+            'id': 1,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 2,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 3,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+        {
+            'id': 0,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+        {
+            'id': 1,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 2,
+            'name': "Jogo de Prato",
+            'image': JogoPrato,
+            'comprado': true
+        },
+        {
+            'id': 3,
+            'name': "Jogo de Copos",
+            'image': JogoCopo,
+            'comprado':false
+        },
+    ]
+
 </script>
 
 <template>
-    <div class="logo">
-        <img :src="LogoImage"/>
+    <div class="container">
+        <div class="title">
+            <h1>Lista de Presentes</h1>
+        </div>
+        <div class="gift-list">
+            <ListItem v-for="item in items"
+            :id="item.id"
+            :name-item="item.name"
+            :image-item="item.image"
+            :status="item.comprado"
+            :key="item.id"/>
+        </div>
+        <div class="logo">
+            <img :src="LogoImage"/>
+        </div>
     </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+.gift-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 40px;
+}
+</style>
