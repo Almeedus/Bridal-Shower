8276db46643553e971f6965d9b3a7f0fff71569c
Eduardo Almeida <almeedusa@gmail.com>
2026-06-22 21:45:03 -0300

    Add final carousel images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,41 @@
 <script setup>
-    import InviteImage from '../assets/InviteImage.png'
-    import LogoImage from '../assets/LogoImage.png'
+import InviteImage from '../assets/InviteImage.png'
+import LogoImage from '../assets/LogoImage.png'
+import AnoNovoImage from '../assets/ano-novo.JPG'
+import AparecidaImage from '../assets/aparecida.jpg'
+import IrmaJoseliImage from '../assets/irma-joseli.JPG'
+import NatalImage from '../assets/natal.JPG'
+import NoivadoImage from '../assets/noivado.JPG'
 </script>
 
-
 <template>
-    <div class="container">
-        <div class="invite">
-            <img :src="InviteImage"/>
-        </div>
-        <div class="whatsapp-button">
-            <a href="https://chat.whatsapp.com/Ji8ugBsbG2sJliP2qIaYVC">
-                Entre no nosso grupo do Whatsapp
-            </a>
-        </div>
-        <div class="photo-carousel">
-            <div class="track-images">
-                <img :src="LogoImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-                <img :src="LogoImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-                <img :src="InviteImage"/>
-            </div>
-        </div>
-        <div class="logo">
-            <img :src="LogoImage"/>
-        </div>
+  <div class="container">
+    <div class="invite">
+      <img :src="InviteImage" />
     </div>
+    <div class="whatsapp-button">
+      <a href="https://chat.whatsapp.com/Ji8ugBsbG2sJliP2qIaYVC">
+        Entre no nosso grupo do Whatsapp
+      </a>
+    </div>
+    <div class="photo-carousel">
+      <div class="track-images">
+        <img :src="AnoNovoImage" />
+        <img :src="AparecidaImage" />
+        <img :src="IrmaJoseliImage" />
+        <img :src="NatalImage" />
+        <img :src="NoivadoImage" />
+        <img :src="AnoNovoImage" />
+        <img :src="AparecidaImage" />
+        <img :src="IrmaJoseliImage" />
+        <img :src="NatalImage" />
+        <img :src="NoivadoImage" />
+      </div>
+    </div>
+    <div class="logo">
+      <img :src="LogoImage" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
-
+<style scoped></style>
