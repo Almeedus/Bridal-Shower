0eca348f5baa8c06c7a6cb3cedc9b536ab4efcc8
Eduardo Almeida <almeedusa@gmail.com>
2026-06-22 21:54:09 -0300

    add message loading state

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -5,73 +5,76 @@ import LogoImage from '../assets/LogoImage.png'
 import ListItem from '@/components/ListItem.vue'
 
 const items = ref([])
+const loading = ref(true)
 
 async function loadItems() {
+  loading.value = true
 
-    const response = await fetch(
-        'https://bridal-shower-backend-0s12.onrender.com/items'
-    )
+  const response = await fetch('https://bridal-shower-backend-0s12.onrender.com/items')
 
-    const data = await response.json()
+  const data = await response.json()
 
-    items.value = data
+  items.value = data
+
+  loading.value = false
 }
 
 async function changeStatus(id) {
+  const answer = confirm('Confirme a escolha do item.')
 
-    const answer = confirm('Confirme a escolha do item.')
+  if (!answer) {
+    return
+  }
 
-    if (!answer) {
-        return
-    }
+  await fetch(`https://bridal-shower-backend-0s12.onrender.com/items/${id}`, {
+    method: 'PATCH',
+  })
 
-    await fetch(
-        `https://bridal-shower-backend-0s12.onrender.com/items/${id}`,
-        {
-            method: 'PATCH'
-        }
-    )
-
-    await loadItems()
+  await loadItems()
 }
 
 onMounted(() => {
-    loadItems()
+  loadItems()
 })
 </script>
 
 <template>
-    <div class="container">
-
-        <div class="title">
-            <h1>Lista de Presentes</h1>
-        </div>
-
-        <div class="gift-list">
-
-            <ListItem
-                v-for="item in items"
-                :key="item.id"
-                :id="item.id"
-                :name-item="item.name"
-                :image-item="item.image"
-                :status="item.comprado"
-                @card-click="!item.comprado && changeStatus(item.id)"
-            />
-
-        </div>
-
-        <div class="logo">
-            <img :src="LogoImage"/>
-        </div>
-
+  <div class="container">
+    <div class="title">
+      <h1>Lista de Presentes</h1>
     </div>
+
+    <div v-if="loading" class="loading">Carregando lista de presentes...</div>
+    <div v-if="loading" class="loading">(Caso demore atualize a página)</div>
+
+    <div class="gift-list">
+      <ListItem
+        v-for="item in items"
+        :key="item.id"
+        :id="item.id"
+        :name-item="item.name"
+        :image-item="item.image"
+        :status="item.comprado"
+        @card-click="!item.comprado && changeStatus(item.id)"
+      />
+    </div>
+
+    <div class="logo">
+      <img :src="LogoImage" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .gift-list {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 40px;
+}
+
+.loading {
+  text-align: center;
+  font-size: 1.2rem;
+  color: #53674d;
 }
 </style>
